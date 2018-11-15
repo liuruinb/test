1b62dc2878989671d91cd32024c53375557f09cb
lixiaowei <m17778046644@163.com>
2018-11-15 17:41:58 +0800

    测试

---
+++ src/main/java/lige/lige/Controller/HomeController.java
@@ -0,0 +1,16 @@
+package lige.lige.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController
+{
+    @ResponseBody
+    @RequestMapping(value = "/index")
+    public String index()
+    {
+        return "hello xiao wei";
+    }
+}
---
+++ src/main/java/lige/lige/LigeApplication.java
@@ -0,0 +1,12 @@
+package lige.lige;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LigeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LigeApplication.class, args);
+	}
+}
