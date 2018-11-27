6c910cf5f0429cd8f86a065d57e0a2bd8265e4dd
liurui <liurui@0591city.com.cn>
2018-11-27 10:58:38 +0800

    提交代码此代码都不可用只是test

---
+++ src/main/java/lige/lige/Model/ChlibProxy.java
@@ -0,0 +1,16 @@
+package lige.lige.Model;
+
+import org.springframework.cglib.proxy.MethodInterceptor;
+import org.springframework.cglib.proxy.MethodProxy;
+
+import java.lang.reflect.Method;
+
+/**
+ * Created by liurui on 2018/11/27.
+ */
+public class ChlibProxy implements MethodInterceptor {
+    @Override
+    public Object intercept(Object o, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
+        return null;
+    }
+}
